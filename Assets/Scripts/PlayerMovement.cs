b1946b8b28e0ff939ffa0cf8263845d9d5edda9d
tarmodev <tarmo.pelifarmi@gmail.com>
2021-09-29 09:51:06 +0300

    Add horizontal multiplier

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,13 +12,19 @@ public class PlayerMovement : MonoBehaviour
 
     public float speed = 5;
     public Rigidbody rb;
+    public float horizontalMultiplier = 2;
 
     float horizontalInput;
 
     private void FixedUpdate()
     {
         Vector3 forwardMove = transform.forward * speed * Time.fixedDeltaTime;
-        Vector3 horizontalMove = transform.right * horizontalInput * speed * Time.fixedDeltaTime;
+        Vector3 horizontalMove = 
+            transform.right 
+            * horizontalInput 
+            * speed 
+            * Time.fixedDeltaTime
+            * horizontalMultiplier;
         rb.MovePosition(rb.position + forwardMove + horizontalMove);
         
     }
