28d8ec667ce1b5c8835fe097bffb14f47fa40e70
tarmodev <tarmo.pelifarmi@gmail.com>
2021-09-29 09:38:05 +0300

    Remove test script. Make player fall to the ground.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+//using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //void Start()
+    //{
+    //
+    //}
+
+    public float speed = 5;
+    public Rigidbody rb;
+
+    private void FixedUpdate()
+    {
+        Vector3 forwardMove = transform.forward * speed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + forwardMove);
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
