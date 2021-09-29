e784c1cf6d2195f55ed42805e3e7207dbfb98677
tarmodev <tarmo.pelifarmi@gmail.com>
2021-09-29 12:32:22 +0300

    Added obstacles.

--- Assets/Scripts/GroundTile.cs
+++ Assets/Scripts/GroundTile.cs
@@ -3,16 +3,18 @@
 using UnityEngine;
 
 public class GroundTile : MonoBehaviour {
+    public GameObject obstaclePrefab;
+
     GroundSpawner groundSpawner;
 
     // Start is called before the first frame update
     void Start() {
         groundSpawner = GameObject.FindObjectOfType<GroundSpawner>();
+        SpawnObstacle();
 
     }
 
-    private void OnTriggerExit(Collider other)
-    {
+    private void OnTriggerExit(Collider other) {
         groundSpawner.SpawnTile();
         Destroy(gameObject, 2);
 
@@ -23,4 +25,12 @@ public class GroundTile : MonoBehaviour {
         
     }
 
+    void SpawnObstacle() {
+        // Choose a random point to spawn the obstacle 
+        int obstacleSpawnIndex = Random.Range(2, 5);
+        Transform spawnPoint = transform.GetChild(obstacleSpawnIndex).transform;
+        // Spawn the obstacle at the position
+        Instantiate(obstaclePrefab, spawnPoint.position, Quaternion.identity, transform);
+
+    }
 }
