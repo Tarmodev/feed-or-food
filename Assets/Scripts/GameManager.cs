bf9d22e7f0d7a4f0a198448192caa570eee2684d
tarmodev <tarmo.pelifarmi@gmail.com>
2021-09-30 13:19:48 +0300

    Work on score and score display.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,14 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
+//using System.Collections;
+//using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public int score;
+    public Text scoreText;
+
     public static GameManager inst;
+
     private void Awake()
     {
+        // Create singleton.
         inst = this;
     }
+
+    private void updateScoreText()
+    {
+        scoreText.text = "Food: " + score;
+    }
+
+    public void IncrementScore()
+    {
+        score++;
+        updateScoreText();
+
+    }
+
 
     // Start is called before the first frame update
     void Start()
