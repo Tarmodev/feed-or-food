dba762f1f7743365b1e5a51cd3ff58c03e7826b1
tarmodev <tarmo.pelifarmi@gmail.com>
2021-10-06 14:16:41 +0300

    Edit PlayerMovement script

--- Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -8,21 +8,26 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float horizontalMoveSpeed = 5.0f;
     [SerializeField] private float maxSpeed = 20.0f;
     [SerializeField] private float rotateSpeed = 2.0f;
-    //[SerializeField] private float jumpForce = 2.0f;
+    [SerializeField] private Transform jumpCheckPos;
+    [SerializeField] private float jumpForce = 2.0f;
+    [SerializeField] private float jumpTime = 1.0f;
 
     private Rigidbody rb;
 
     private float horizontalMoveInput;
     private bool jumpInput;
 
-    [SerializeField] private Transform checkPos;
-    private float checkRadius;
-
-    private bool isGrounded;
-    private int whatIsGround;
     private KeyCode jumpButton = KeyCode.Space;
     private string horizontalAxis = "Horizontal";
 
+    [SerializeField] private float checkRadius;
+
+    private bool isGrounded;
+    public LayerMask whatIsGround;
+
+    private float jumpTimeCounter;
+
+    private bool isJumping = false;
 
 
     private void UpdateInput()
@@ -34,9 +39,9 @@ public class PlayerMovement : MonoBehaviour
 
     private void MovePlayer()
     {
-        Debug.Log("MovePlaýer()");
+        //Debug.Log("MovePlayer()");
         transform.Rotate(Vector3.up * horizontalMoveInput * rotateSpeed);
-        if (rb.velocity.magnitude < maxSpeed)
+        if(rb.velocity.magnitude < maxSpeed)
         {
             rb.AddForce((transform.forward) * horizontalMoveSpeed);
 
@@ -44,9 +49,16 @@ public class PlayerMovement : MonoBehaviour
     
     }
 
+    private bool CheckIfIsGrounded()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(jumpCheckPos.position, checkRadius, whatIsGround);
+        return hitColliders != null;
+
+    }
+
     private void Jump()
     {
-        throw new NotImplementedException();
+        rb.velocity = Vector3.up * jumpForce;
 
     }
 
@@ -62,14 +74,35 @@ public class PlayerMovement : MonoBehaviour
     }
 
     private void FixedUpdate() {
-        Collider[] hitColliders = Physics.OverlapSphere(checkPos.position, checkRadius, whatIsGround);
-        if (hitColliders != null && jumpInput)
+
+        isGrounded = CheckIfIsGrounded();
+        if(isGrounded && jumpInput)
         {
             Jump();
+            Debug.Log("Jumped();");
+
         }
 
         MovePlayer();
+        if(jumpInput)
+        {
+            if(jumpTimeCounter < 0.0f)
+            {
+                rb.velocity = Vector3.up * jumpForce;
+                jumpTime -= Time.deltaTime;
 
+            }
+            else
+            {
+                isJumping = false;
+            }
+
+        }
+        
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            isJumping = false;
+        }
     }
 
 }
